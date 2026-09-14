c418aa83936b0326189fa95c098ad5a9d3146531
TheInfani <dlaspama89@gmail.com>
2026-09-14 11:24:41 +0300

    First Commit

---
+++ 1lab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 18;
+    int b = 4;
+    int c = -3;
+
+    int result = a + b * c;
+    printf("Hello for everyone apple!\n");
+    printf("%d\n", result);
+    return 0;
+}
+
